8d3cadc0af0fb0d91aac01543899d7ea8471a8fa
root <root@applejack.ecsig.com>
2013-09-18 17:36:59 +0000

    Working now?

--- git.php
+++ git.php
@@ -10,10 +10,9 @@
         // The commands
 	chdir('/srv/www/ecsig.com/public_html_git'); 
        $commands = array(
-//		'cd /srv/www/ecsig.com/public_html_git',
                 'echo $PWD',
                 'whoami',
-                'git pull',
+		'git reset --hard origin/master',
                 'git status',
                 'git submodule sync',
                 'git submodule update',
